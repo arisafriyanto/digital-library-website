9ae70749541024d0d4fbf80d286c2bba0693942d
arisafriyanto <arisafriyanto1933@gmail.com>
2023-07-24 13:49:29 +0700

    add read book and delete buton in books.show

--- resources/views/books/show.blade.php
+++ resources/views/books/show.blade.php
@@ -4,20 +4,41 @@
             <div class="col-md-12">
                 <h1>{{ $book->book_title }}</h1>
 
-                <div class="text-secondary mb-3">
-                    <a href="{{ route('categories.show', $book->category->slug) }}">{{ $book->category->name }}</a>
-                    <span>&middot; {{ $book->created_at->format('d F, Y') }}</span>
 
-                    <div class="d-flex align-items-center my-3">
-                        <div class="flex-shrink-0">
-                            <img src="{{ $book->user->gravatar() }}" class="rounded-circle" alt="Profile" width="60">
-                        </div>
-                        <div class="flex-grow-1 ms-3">
-                            <div>
-                                {{ $book->user->name }}
+                <div class="d-flex justify-content-between">
+                    <div class="text-secondary mb-3">
+                        <a href="{{ route('categories.show', $book->category->slug) }}">{{ $book->category->name }}</a>
+                        <span>&middot; {{ $book->created_at->format('d F, Y') }}</span>
+
+                        <div class="d-flex align-items-center my-3">
+                            <div class="flex-shrink-0">
+                                <img src="{{ $book->user->gravatar() }}" class="rounded-circle" alt="Profile"
+                                    width="60">
                             </div>
-                            {{ '@' . $book->user->username }}
+                            <div class="flex-grow-1 ms-3">
+                                <div>
+                                    {{ $book->user->name }}
+                                </div>
+                                {{ '@' . $book->user->username }}
+                            </div>
                         </div>
+                    </div>
+
+                    <div class="d-flex">
+                        <div>
+                            <a href="{{ asset('storage/' . $book->book_file) }}" target="_blank"
+                                class="btn btn-success me-2">Read Book</a>
+                        </div>
+
+
+                        <form action="{{ route('books.destroy', $book->id) }}" method="post">
+                            @csrf
+                            @method('DELETE')
+
+                            <button type="submit" class="btn btn-danger"
+                                onclick="return confirm('Are you sure you want to delete book data?')">Delete
+                            </button>
+                        </form>
                     </div>
                 </div>
 
